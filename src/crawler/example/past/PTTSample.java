1caeaa5d575eb21d1ec44b30cf76fa0ff6cee9e6
dayu <dayu@dayu>
2017-12-10 12:01:43 +0800

    dayu commit add sample

--- src/crawler/example/past/PTTSample.java
+++ src/crawler/example/past/PTTSample.java
@@ -17,25 +17,23 @@ public class PTTSample {
 
 		// 遠端資料路徑
 		String uri = "https://www.ptt.cc/bbs/marriage/M.1512832927.A.535.html";
-        Elements jj = CrawlerPack.start().addCookie("over18", "1").getFromHtml(uri).select("#main-content");
-        System.out.println(jj.select("span,div").remove().text());
-
+		// =============================================取主文======================================================//
+		Elements jj = CrawlerPack.start().addCookie("over18", "1").getFromHtml(uri).select("#main-content");
+		System.out.println(jj.select("span,div").remove().text().substring(11));
+		// =============================================取主文======================================================//
 		System.out.println(CrawlerPack.start()
-
 				// 參數設定
 				.addCookie("over18", "1") // 設定cookie 要先用瀏覽器 檢察cookies 是不適
 				// .setRemoteEncoding("big5")// 設定遠端資料文件編碼
-
 				// 選擇資料格式 (三選一)
-				.getFromHtml(uri)
-				 .select(".hl.push-tag:contains(推)+span")
-				// .select(".hl.push-tag:contains(推)")
-//				.select("#maib-context")
-//		 .getFromHtml(uri)
-//		 .getFromXml(uri)
+				.getFromHtml(uri).select(".hl.push-tag:contains(推)+span")
+		// .select(".hl.push-tag:contains(推)")
+		// .select("#maib-context")
+		// .getFromHtml(uri)
+		// .getFromXml(uri)
 
-//		 這兒開始是 Jsoup Document 物件操作
-//		 .select(".css .selector ")
+		// 這兒開始是 Jsoup Document 物件操作
+		// .select(".css .selector ")
 
 		);
 	}

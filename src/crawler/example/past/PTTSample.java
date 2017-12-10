0a8ba538651cef5acf9aa4a18edd88fa4a280043
dayu <dayu@dayu>
2017-12-10 12:03:56 +0800

    dayu commit add sample

--- src/crawler/example/past/PTTSample.java
+++ src/crawler/example/past/PTTSample.java
@@ -6,35 +6,36 @@ import org.apache.commons.logging.impl.SimpleLog;
 import org.jsoup.select.Elements;
 
 public class PTTSample {
-	// commit test test
-	public static void main(String[] args) {
+    // commit test test
+    public static void main(String[] args) {
 
-		// set to debug level
-		// CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+        // set to debug level
+        // CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
 
-		// turn off logging
-		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+        // turn off logging
+        CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
-		// 遠端資料路徑
-		String uri = "https://www.ptt.cc/bbs/marriage/M.1512832927.A.535.html";
-		// =============================================取主文======================================================//
-		Elements jj = CrawlerPack.start().addCookie("over18", "1").getFromHtml(uri).select("#main-content");
-		System.out.println(jj.select("span,div").remove().text().substring(11));
-		// =============================================取主文======================================================//
-		System.out.println(CrawlerPack.start()
-				// 參數設定
-				.addCookie("over18", "1") // 設定cookie 要先用瀏覽器 檢察cookies 是不適
-				// .setRemoteEncoding("big5")// 設定遠端資料文件編碼
-				// 選擇資料格式 (三選一)
-				.getFromHtml(uri).select(".hl.push-tag:contains(推)+span")
-		// .select(".hl.push-tag:contains(推)")
-		// .select("#maib-context")
-		// .getFromHtml(uri)
-		// .getFromXml(uri)
+        // 遠端資料路徑
+        String uri = "https://www.ptt.cc/bbs/marriage/M.1512832927.A.535.html";
+        // =============================================取主文======================================================//
+        Elements jsoup = CrawlerPack.start().addCookie("over18", "1").getFromHtml(uri).select("#main-content");
+        System.out.println(jsoup.select("span,div").remove().text().substring(11));
+        System.out.println("========================================================================================");
+        // =============================================取主文======================================================//
+        System.out.println(CrawlerPack.start()
+                        // 參數設定
+                        .addCookie("over18", "1") // 設定cookie 要先用瀏覽器 檢察cookies 是不適
+                        // .setRemoteEncoding("big5")// 設定遠端資料文件編碼
+                        // 選擇資料格式 (三選一)
+                        .getFromHtml(uri).select(".hl.push-tag:contains(推)+span,.hl.push-tag:contains(噓)+span")
+                // .select(".hl.push-tag:contains(推)")
+                // .select("#maib-context")
+                // .getFromHtml(uri)
+                // .getFromXml(uri)
 
-		// 這兒開始是 Jsoup Document 物件操作
-		// .select(".css .selector ")
+                // 這兒開始是 Jsoup Document 物件操作
+                // .select(".css .selector ")
 
-		);
-	}
+        );
+    }
 }
